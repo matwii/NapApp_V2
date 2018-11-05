a17eb7ee0a0c39034c26d0a7119a3064a8c4c4a6
Matz Wiik <matzwiik@gmail.com>
2018-11-05 16:15:07 +0100

    Style changes to login component

--- components/login-component/login-component.js
+++ components/login-component/login-component.js
@@ -1,38 +1,46 @@
 import React from 'react';
-import { View, TextInput, TouchableHighlight, Text, Image } from 'react-native';
+import {View, TextInput, TouchableHighlight, Text, Image} from 'react-native';
+import {SocialIcon, Button} from 'react-native-elements'
 import styles from './styles';
 
 const LoginComponent = () => (
-  <View style={styles.container}>
-    <View style={styles.imageContainer}>
-      <Image
-        style={{ width: 80, height: 80 }}
-        source={require('../car-component/car.png')}
-      />
+    <View style={styles.container}>
+        <TextInput
+            placeholder="Username"
+            style={styles.input}
+        />
+        <TextInput
+            placeholder="Password"
+            style={styles.input}
+            secureTextEntry
+        />
+        <Button
+            title='Log in'
+            rounded
+            buttonStyle={{
+                backgroundColor: "rgb(30,144,255)"
+            }}
+        />
+        <View style={styles.bottomContainer}>
+            <TouchableHighlight>
+                <Text style={styles.linkText}>Sign up</Text>
+            </TouchableHighlight>
+            <TouchableHighlight>
+                <Text style={styles.linkText}>Forgotten password?</Text>
+            </TouchableHighlight>
+        </View>
+        <SocialIcon
+            title='Sign In With Facebook'
+            button
+            type='facebook'
+            onPress={() => console.log('onPress')}
+        />
+        <SocialIcon
+            title='Sign In With Google'
+            button
+            type='google-plus-official'
+        />
     </View>
-    <TextInput
-      placeholder="Username"
-      style={styles.input}
-    />
-    <TextInput
-      placeholder="Password"
-      style={styles.input}
-      secureTextEntry
-    />
-    <TouchableHighlight
-      style={styles.okButton}
-    >
-      <Text style={styles.buttonText}>OK</Text>
-    </TouchableHighlight>
-    <View style={styles.bottomContainer}>
-      <TouchableHighlight>
-        <Text style={styles.linkText}>Sign up</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={styles.linkText}>Forgotten password?</Text>
-      </TouchableHighlight>
-    </View>
-  </View>
 );
 
 
