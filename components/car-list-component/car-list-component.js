8292c1619df79eb736a0b6830ee2e4194c4b4d62
Matz Wiik <matzwiik@gmail.com>
2018-11-22 12:57:52 +0100

    authentication

--- components/car-list-component/car-list-component.js
+++ components/car-list-component/car-list-component.js
@@ -3,49 +3,49 @@ import PropTypes from 'prop-types';
 import CarComponent from '../car-component/car-component';
 
 
-const CarListComponent = ({ cars, bookedCar, carBooked }) => {
-  if (carBooked) {
+const CarListComponent = ({cars, bookedCar, carBooked}) => {
+    if (carBooked) {
+        return (
+            <CarComponent
+                coordinates={bookedCar.coordinate}
+                id={bookedCar.id}
+                regNr={bookedCar.regNr}
+                {...bookedCar}
+                key={bookedCar.id}
+            />
+        );
+    }
     return (
-      <CarComponent
-        coordinates={bookedCar.coordinate}
-        id={bookedCar.id}
-        regNr={bookedCar.regNr}
-        {...bookedCar}
-        key={bookedCar.id}
-      />
+        cars.map(car => (
+            <CarComponent
+                coordinates={car.coordinate}
+                id={car.id}
+                regNr={car.regNr}
+                {...car}
+                key={car.id}
+            />
+        ))
     );
-  }
-  return (
-    cars.map(car => (
-      <CarComponent
-        coordinates={car.coordinate}
-        id={car.id}
-        regNr={car.regNr}
-        {...car}
-        key={car.id}
-      />
-    ))
-  );
 };
 
 CarListComponent.propTypes = {
-  cars: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    regNr: PropTypes.string,
-    coordinates: PropTypes.shape({
-      latitude: PropTypes.number,
-      longitude: PropTypes.number,
+    cars: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        regNr: PropTypes.string,
+        coordinates: PropTypes.shape({
+            latitude: PropTypes.number,
+            longitude: PropTypes.number,
+        }),
+    })),
+    bookedCar: PropTypes.shape({
+        id: PropTypes.number,
+        regNr: PropTypes.string,
+        coordinates: PropTypes.shape({
+            latitude: PropTypes.number,
+            longitude: PropTypes.number,
+        }),
     }),
-  })),
-  bookedCar: PropTypes.shape({
-    id: PropTypes.number,
-    regNr: PropTypes.string,
-    coordinates: PropTypes.shape({
-      latitude: PropTypes.number,
-      longitude: PropTypes.number,
-    }),
-  }),
-  carBooked: PropTypes.bool.isRequired,
+    carBooked: PropTypes.bool.isRequired,
 };
 
 export default CarListComponent;
--- components/login-component/login-component.js
+++ components/login-component/login-component.js
@@ -1,39 +1,64 @@
 import React from 'react';
-import { View, TextInput, TouchableHighlight, Text, Image } from 'react-native';
+import { View, TextInput, TouchableHighlight, Text, Image, Alert } from 'react-native';
 import styles from './styles';
 
-const LoginComponent = () => (
-  <View style={styles.container}>
-    <View style={styles.imageContainer}>
-      <Image
-        style={{ width: 80, height: 80 }}
-        source={require('../car-component/car.png')}
-      />
-    </View>
-    <TextInput
-      placeholder="Username"
-      style={styles.input}
-    />
-    <TextInput
-      placeholder="Password"
-      style={styles.input}
-      secureTextEntry
-    />
-    <TouchableHighlight
-      style={styles.okButton}
-    >
-      <Text style={styles.buttonText}>OK</Text>
-    </TouchableHighlight>
-    <View style={styles.bottomContainer}>
-      <TouchableHighlight>
-        <Text style={styles.linkText}>Sign up</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={styles.linkText}>Forgotten password?</Text>
-      </TouchableHighlight>
-    </View>
-  </View>
-);
+class LoginComponent extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            isAuthenticated: false,
+            user: null,
+            token: ''
+        }
+    }
+
+    onFailure = (error) => {
+        Alert(error);
+    };
+
+    facebookResponse = (response) => {
+        console.log(response);
+    };
+
+    googleResponse = (response) => {
+        console.log(response);
+    };
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        style={{width: 80, height: 80}}
+                        source={require('../car-component/car.png')}
+                    />
+                </View>
+                <TextInput
+                    placeholder="Username"
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Password"
+                    style={styles.input}
+                    secureTextEntry
+                />
+                <TouchableHighlight
+                    style={styles.okButton}
+                >
+                    <Text style={styles.buttonText}>OK</Text>
+                </TouchableHighlight>
+                <View style={styles.bottomContainer}>
+                    <TouchableHighlight>
+                        <Text style={styles.linkText}>Sign up</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight>
+                        <Text style={styles.linkText}>Forgotten password?</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+        )
+    }
+}
 
 
 export default LoginComponent;
